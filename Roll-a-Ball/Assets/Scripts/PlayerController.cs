95fb4d34a675db9e442f278adf50b638edd7f4a1
alakijaayo <alakijaayo@gmail.com>
2019-01-28 13:07:00 +0000

    Displaying the Score and Text

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
   public float speed;
+  public Text countText;
+  public Text winText;
   private Rigidbody rb;
+  private int count;
 
   void Start()
   {
     rb = GetComponent<Rigidbody>();
+    count = 0;
+    SetCountText();
+    winText.text = "";
   }
 
   void Update()
@@ -32,6 +39,17 @@ public class PlayerController : MonoBehaviour
     if (other.gameObject.CompareTag("Pick Up"))
     {
       other.gameObject.SetActive(false);
+      count = count + 1;
+      SetCountText();
+    }
+  }
+
+  void SetCountText()
+  {
+    countText.text = "Count: " + count.ToString();
+    if(count >= 12)
+    {
+      winText.text = "You Win!";
     }
   }
 }
